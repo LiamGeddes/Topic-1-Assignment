070a596cb6b22766708fa3a428ff13cfe0c2e10b
Liam Geddes <362675837@GoTVDSB.ca>
2026-09-17 13:20:09 -0400

    Almost Done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Topic_1_Assignment
+﻿using System.Text.Json;
+
+namespace Topic_1_Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, My name is Liam");
+            //Comments are used to keep track of where you left off or it's to tell something to the reader.'
+
+            Console.WriteLine("L      GGG"); 
+            Console.WriteLine("L     G   G");
+            Console.WriteLine("L     G");
+            Console.WriteLine("L     GGGGG");
+            Console.WriteLine("L     G    G");
+            Console.WriteLine("L     G    G");
+            Console.WriteLine("LLLLL  GGG");
+                     
         }
     }
 }
