0486a3ee80b00ffc74671ee9b2b18134661927c5
Liam Geddes <362675837@GoTVDSB.ca>
2026-09-18 11:20:46 -0400

    Finished

--- Program.cs
+++ Program.cs
@@ -8,15 +8,16 @@ namespace Topic_1_Assignment
         {
             Console.WriteLine("Hello, My name is Liam");
             //Comments are used to keep track of where you left off or it's to tell something to the reader.'
-
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("L      GGG"); 
-            Console.WriteLine("L     G   G");
+            Console.WriteLine("L     G    G");
             Console.WriteLine("L     G");
             Console.WriteLine("L     GGGGG");
             Console.WriteLine("L     G    G");
             Console.WriteLine("L     G    G");
             Console.WriteLine("LLLLL  GGG");
-                     
+                    
         }
     }
 }
